2340587c1151b7fac56df7cfaa4e917af2e56f4f
KyanReubenNaidoo <ST10439239@vcconnect.edu.za>
2025-09-17 11:41:06 +0200

    Add project files.

---
+++ POEProg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POEProg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
