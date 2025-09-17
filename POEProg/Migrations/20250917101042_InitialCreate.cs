6f42231351e8a62e1d3dbc394e02ece06031ecff
KyanReubenNaidoo <ST10439239@vcconnect.edu.za>
2025-09-17 12:11:58 +0200

    Added Migration to database and updated database

---
+++ POEProg/Migrations/20250917101042_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POEProg.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lecturer",
+                columns: table => new
+                {
+                    LecturerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lecturer", x => x.LecturerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Claim",
+                columns: table => new
+                {
+                    ClaimId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LecturerId = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    TotalAmount = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubmissionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claim", x => x.ClaimId);
+                    table.ForeignKey(
+                        name: "FK_Claim_Lecturer_LecturerId",
+                        column: x => x.LecturerId,
+                        principalTable: "Lecturer",
+                        principalColumn: "LecturerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Approval",
+                columns: table => new
+                {
+                    ApprovalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApprovedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClaimId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Approval", x => x.ApprovalId);
+                    table.ForeignKey(
+                        name: "FK_Approval_Claim_ClaimId",
+                        column: x => x.ClaimId,
+                        principalTable: "Claim",
+                        principalColumn: "ClaimId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    DocumentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClaimId = table.Column<int>(type: "int", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.DocumentId);
+                    table.ForeignKey(
+                        name: "FK_Document_Claim_ClaimId",
+                        column: x => x.ClaimId,
+                        principalTable: "Claim",
+                        principalColumn: "ClaimId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Approval_ClaimId",
+                table: "Approval",
+                column: "ClaimId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Claim_LecturerId",
+                table: "Claim",
+                column: "LecturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_ClaimId",
+                table: "Document",
+                column: "ClaimId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Approval");
+
+            migrationBuilder.DropTable(
+                name: "Document");
+
+            migrationBuilder.DropTable(
+                name: "Claim");
+
+            migrationBuilder.DropTable(
+                name: "Lecturer");
+        }
+    }
+}
